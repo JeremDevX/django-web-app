620b193a8bb66f8d32024863d0a50d3bfa93e489
JeremDevX <153821929+JeremDevX@users.noreply.github.com>
2025-12-17 09:52:11 +0100

    Basic CRUD Operation, with personnalized admin display

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,8 +1,40 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
-    name = models.fields.CharField(max_length=100)
 
+    def __str__(self):
+        return f'{self.name}'
+
+    class Genre(models.TextChoices):
+        HIP_HOP ='HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+    name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2025)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Type(models.TextChoices):
+        RECORDS = 'RE'
+        CLOTHING = 'CL'
+        MISCELLANEOUS = 'MI'
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=150)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2025)]
+    )
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
+
