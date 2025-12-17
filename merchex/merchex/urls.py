982b391fab441f4608bfe148965fb1aa5ac52a38
JeremDevX <153821929+JeremDevX@users.noreply.github.com>
2025-12-17 11:57:54 +0100

    Add contact form functionality with email sending capability

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('bands/', views.band_list, name='band-list'),
     path('bands/<int:id>/', views.band_detail, name='band-detail'),
     path('about-us/', views.about),
-    path('contact-us/', views.contact),
+    path('contact-us/', views.contact, name='contact'),
     path('listings/', views.listings),
 ]
