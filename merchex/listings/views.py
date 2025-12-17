38ba0584c0302f152be82ee1d67f7c15ec1e3766
JeremDevX <153821929+JeremDevX@users.noreply.github.com>
2025-12-17 14:39:53 +0100

    Implement CRUD functionality for Band model with corresponding forms and templates

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band, Listing
-from listings.forms import ContactUsForm
+from listings.forms import ContactUsForm, BandForm
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 
@@ -14,6 +14,45 @@ def band_detail(request, id):
     band = Band.objects.get(id=id)
     return render(request,
                   'listings/band_detail.html',
+                  {'band': band})
+
+def band_create(request):
+    if request.method == "POST":
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm()
+
+    return render(request,
+                  'listings/band_create.html',
+                  {'form': form})
+
+def band_update(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == "POST":
+        form = BandForm(request.POST, instance = band)
+        if form.is_valid():
+            form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
+    
+    return render(request,
+                  'listings/band_update.html',
+                  {'form': form})
+
+def band_delete(request, id):
+    band = Band.objects.get(id = id)
+
+    if request.method == "POST":
+        band.delete()
+        return redirect('band-list')
+
+    return render(request,
+                  'listings/band_delete.html',
                   {'band': band})
 
 def about(request):
