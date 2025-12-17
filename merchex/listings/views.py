1ed763f8cb05ad0dc2a61c9a79908e83007ea79f
JeremDevX <153821929+JeremDevX@users.noreply.github.com>
2025-12-17 11:24:30 +0100

    Implement band listing and detail views with corresponding templates

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,10 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band, Listing
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 'listings/hello.html',
+    return render(request, 'listings/band_list.html',
                   {'bands': bands})
+
+def band_detail(request, id):
+    band = Band.objects.get(id=id)
+    return render(request,
+                  'listings/band_detail.html',
+                  {'band': band})
 
 def about(request):
     return render(request, 'listings/about.html')
